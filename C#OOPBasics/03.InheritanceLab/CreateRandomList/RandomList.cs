128770191f1ec940cb13fd2c66ab99c00510705a
ivanlutov <ivanlutov@abv.bg>
2017-07-04 21:31:58 +0300

    Clear any whitespaces

--- C#OOPBasics/03.InheritanceLab/CreateRandomList/RandomList.cs
+++ C#OOPBasics/03.InheritanceLab/CreateRandomList/RandomList.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections;
-
 public class RandomList : ArrayList
 {
     private Random rnd;
-
     public RandomList()
     {
         this.rnd = new Random();
     }
-
     public string RandomString()
     {
         var index = this.rnd.Next(base.Count - 1);
