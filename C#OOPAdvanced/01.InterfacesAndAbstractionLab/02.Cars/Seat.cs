862683614f91c931cf6da24931f9736dfa592911
ivanlutov <ivanlutov@abv.bg>
2017-07-18 18:38:47 +0300

    Add InterfacesAndAbstractionLab

---
+++ C#OOPAdvanced/01.InterfacesAndAbstractionLab/02.Cars/Seat.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+public class Seat : ICar
+{
+    public string Model { get; private set; }
+    public string Color { get; private set; }
+
+    public Seat(string model, string color)
+    {
+        Model = model;
+        Color = color;
+    }
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"{this.Color} {GetType().Name} {this.Model}");
+        sb.AppendLine(this.Start());
+        sb.AppendLine(this.Stop());
+
+        return sb.ToString().Trim();
+    }
+}
---
+++ C#OOPAdvanced/01.InterfacesAndAbstractionLab/02.Cars/Tesla.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+public class Tesla : ICar, IElectricCar
+{
+    public string Model { get; private set; }
+    public string Color { get; private set; }
+    public int Battery { get; private set; }
+
+    public Tesla(string model, string color, int battery)
+    {
+        Model = model;
+        Color = color;
+        Battery = battery;
+    }
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"{this.Color} {GetType().Name} {this.Model} with {this.Battery} Batteries");
+        sb.AppendLine(this.Start());
+        sb.AppendLine(this.Stop());
+
+        return sb.ToString().Trim();
+    }
+}
