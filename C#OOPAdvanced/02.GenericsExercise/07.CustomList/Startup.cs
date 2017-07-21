cab11cfcf1192d1636bd69ae3ea9f4cbce84a9f5
ivanlutov <ivanlutov@abv.bg>
2017-07-21 23:03:03 +0300

    Add exercises

---
+++ C#OOPAdvanced/02.GenericsExercise/07.CustomList/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _07.CustomList
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var interpretator = new CommandInterpretator();
+
+            var input = Console.ReadLine();
+            while (input != "END")
+            {
+                interpretator.ParseCommand(input);
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ C#OOPAdvanced/02.GenericsExercise/08.CustomListSorter/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _08.CustomListSorter
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var interpretator = new CommandInterpretator();
+
+            var input = Console.ReadLine();
+            while (input != "END")
+            {
+                interpretator.ParseCommand(input);
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
