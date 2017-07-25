a99b4fd4450b1ad76484a8227ae2c32fd99bb45d
ivanlutov <ivanlutov@abv.bg>
2017-07-25 16:03:08 +0300

    Add IteratorsAndComparatorsLab

---
+++ C#OOPAdvanced/03.IteratorsAndComparatorsLab/01.Library/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Book : IComparable<Book>
+{
+    public Book(string title, int year, params string[] authors)
+    {
+        this.Title = title;
+        this.Year = year;
+        this.Authors = new List<string>(authors);
+    }
+
+    public string Title { get; set; }
+    public int Year { get; set; }
+    public List<string> Authors { get; set; }
+    public int CompareTo(Book other)
+    {
+        if (this.Year.CompareTo(other.Year) != 0)
+        {
+            return this.Year.CompareTo(other.Year);
+        }
+
+        return this.Title.CompareTo(other.Title);
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Title} - {this.Year}";
+    }
+}
---
+++ C#OOPAdvanced/03.IteratorsAndComparatorsLab/02.LibraryIterator/Startup.cs
@@ -0,0 +1,11 @@
+﻿
+namespace _02.LibraryIterator
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
