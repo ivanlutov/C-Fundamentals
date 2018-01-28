00bccb7eafc7b2969ae9a5e18ef60280b44d762d
ivanlutov <ivanlutov@abv.bg>
2018-01-28 17:08:39 +0200

    Update rubiks matrix

--- C#Advanced/04.MatricesExercise/05.RubiksMatrix/StartUp.cs
+++ C#Advanced/04.MatricesExercise/05.RubiksMatrix/StartUp.cs
@@ -7,71 +7,106 @@ namespace _05.RubiksMatrix
     {
         public static void Main()
         {
-            var matrixSize = Console.ReadLine().Split(new char[] { ' ' }).Select(int.Parse).ToArray();
+            var matrixSize = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
             var row = matrixSize[0];
             var col = matrixSize[1];
 
-            var matrix = new string[row][];
+            var matrix = new int[row][];
+            int counter = 1;
 
-            int countNum = 1;
-            for (int i = 0; i < row; i++)
+            for (int rowIndex = 0; rowIndex < row; rowIndex++)
             {
-                string line = Console.ReadLine().Trim();
-                var currentColumn = new string[line.Length];
-                for (int j = 0; j < line.Length; j++)
+                matrix[rowIndex] = new int[col];
+                for (int colIndex = 0; colIndex < matrix[0].Length; colIndex++)
                 {
-                    currentColumn[j] = line[j].ToString();
+                    matrix[rowIndex][colIndex] = counter;
+                    counter++;
                 }
-                matrix[i] = currentColumn;
             }
-            //for (int rowIndex = 0; rowIndex < row; rowIndex++)
-            //{
-            //    matrix[rowIndex] = new int[col];
 
-            //    for (int colIndex = 0; colIndex < matrix[0].Length; colIndex++)
-            //    {
-            //        matrix[rowIndex][colIndex] = countNum;
-            //        countNum++;
-            //    }
-            //}
-
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 0; i < n; i++)
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfCommands; i++)
             {
-                var line = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                var column = int.Parse(line[0]);
-                var command = line[1];
-                var count = int.Parse(line[2]);
+                var currentLine = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var indexOfRowOrCol = int.Parse(currentLine[0]);
+                var direction = currentLine[1];
+                var moves = int.Parse(currentLine[2]);
 
-                if (command == "up")
+                switch (direction)
                 {
-                    for (int rowIndex = 0; rowIndex < row; rowIndex++)
+                    case "up":
+                        MoveUpAndDown(matrix, indexOfRowOrCol, moves);
+                        break;
+                    case "down":
+                        MoveUpAndDown(matrix, indexOfRowOrCol, row - moves % row);
+                        break;
+                    case "left":
+                        MoveRightAndLeft(matrix, indexOfRowOrCol, moves);
+                        break;
+                    case "right":
+                        MoveRightAndLeft(matrix, indexOfRowOrCol, col - moves % col);
+                        break;
+                }
+            }
+            var element = 1;
+
+            for (int rowIndex = 0; rowIndex < matrix.Length; rowIndex++)
+            {
+                for (int colIndex = 0; colIndex < matrix[0].Length; colIndex++)
+                {
+                    if (matrix[rowIndex][colIndex] == element)
                     {
-                        for (int colIndex = 0; colIndex < matrix[0].Length; colIndex++)
+                        Console.WriteLine("No swap required");
+                    }
+
+                    else
+                    {
+                        for (int r = 0; r < matrix.Length; r++)
                         {
-                            if (colIndex == column)
+                            for (int c = 0; c < matrix[0].Length; c++)
                             {
-                                for (int j = 0; j < count; j++)
+                                if (matrix[r][c] == element)
                                 {
+                                    var currentElement = matrix[rowIndex][colIndex];
+                                    matrix[rowIndex][colIndex] = element;
+                                    matrix[r][c] = currentElement;
+                                    Console.WriteLine($"Swap ({rowIndex}, {colIndex}) with ({r}, {c})");
+                                    break;
                                 }
                             }
                         }
                     }
-                }
-                else if (command == "down")
-                {
-                }
-                else if (command == "right")
-                {
-                }
-                else if (command == "left")
-                {
+                    element++;
                 }
             }
-            //foreach (var rowe in matrix)
-            //{
-            //    Console.WriteLine(string.Join(" ",rowe));
-            //}
+
+        }
+        private static void MoveRightAndLeft(int[][] matrix, int indexOfRowOrCol, int moves)
+        {
+            int[] temp = new int[matrix[0].Length];
+            for (int colIndex = 0; colIndex < matrix[0].Length; colIndex++)
+            {
+                temp[colIndex] = matrix[indexOfRowOrCol][(colIndex + moves) % matrix[0].Length];
+            }
+
+            matrix[indexOfRowOrCol] = temp;
+        }
+
+        private static void MoveUpAndDown(int[][] matrix, int indexOfRowOrCol, int moves)
+        {
+            int[] temp = new int[matrix.Length];
+
+            for (int rowIndex = 0; rowIndex < matrix.Length; rowIndex++)
+            {
+                temp[rowIndex] = matrix[(rowIndex + moves) % matrix.Length][indexOfRowOrCol];
+            }
+
+            for (int rowIndex = 0; rowIndex < matrix.Length; rowIndex++)
+            {
+                matrix[rowIndex][indexOfRowOrCol] = temp[rowIndex];
+            }
         }
     }
+}
 }
